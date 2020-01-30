65e03dadfe3036c220387977296ede4c021a8ab6
Cris Daniluk <cdaniluk@rhythmictech.com>
2020-01-30 12:18:54 -0500

    fix output (#3)
    * fix output

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,9 @@
 output "secret" {
   description = "Generated secret"
-  value       = random_password.random_string.result
+  value       = var.create_secret ? random_password.random_string[0].result : ""
 }
 
 output "secret_arn" {
   description = "The ARN of the secret"
-  value       = aws_secretsmanager_secret.secret.arn
+  value       = var.create_secret ? aws_secretsmanager_secret.secret[0].arn : ""
 }
-
