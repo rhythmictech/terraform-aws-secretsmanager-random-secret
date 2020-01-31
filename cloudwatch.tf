1babffad0e5f64fbe0b7e318f2b2746d813f95ec
Cris Daniluk <cdaniluk@rhythmictech.com>
2020-01-31 11:56:17 -0500

    fix (#4)

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -6,7 +6,7 @@ resource "aws_cloudwatch_log_metric_filter" "secret_access" {
   count          = var.enable_secret_access_notification ? 1 : 0
   name           = "${var.name_prefix}secret-access"
   log_group_name = var.cloudtrail_log_group
-  pattern        = "{ $.eventName = \"GetSecretValue\" && $.requestParameters.secretId = \"${aws_secretsmanager_secret.secret.arn}\" }"
+  pattern        = "{ $.eventName = \"GetSecretValue\" && $.requestParameters.secretId = \"${aws_secretsmanager_secret.secret[0].arn}\" }"
 
   metric_transformation {
     default_value = 0
@@ -20,7 +20,7 @@ resource "aws_cloudwatch_metric_alarm" "unauthorized_cloudtrail_calls" {
   count               = var.enable_secret_access_notification ? 1 : 0
   alarm_actions       = [var.secret_access_notification_arn]
   alarm_name          = "${local.name_prefix}secret-access"
-  alarm_description   = "Monitor usage of secret: ${aws_secretsmanager_secret.secret.id}"
+  alarm_description   = "Monitor usage of secret: ${aws_secretsmanager_secret.secret[0].id}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
   metric_name         = "${local.name_prefix}SecretAccessed"
