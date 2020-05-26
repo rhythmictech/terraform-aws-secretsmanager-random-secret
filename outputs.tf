5cb6bff152ee93c66c99b56f550640f0f0513e27
Steven B <genseb7@gmail.com>
2020-05-26 11:24:44 -0400

    add version id

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "secret_arn" {
   description = "The ARN of the secret"
   value       = var.create_secret ? aws_secretsmanager_secret.secret[0].arn : ""
 }
+
+output "version_id" {
+  description = "The unique identifier of the version of the secret."
+  value       = var.create_secret ? aws_secretsmanager_secret_version.secret_val[0].version_id : ""
+}
