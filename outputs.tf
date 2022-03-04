9fb44fbab2e11ac9898de39ae66dd5aa548028c9
matty87a <matty87a@gmail.com>
2022-03-04 17:07:55 +0000

    Marked secret output as sensitive (#12)

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
 output "secret" {
   description = "Generated secret"
   value       = var.create_secret ? random_password.random_string[0].result : ""
+  sensitive   = true
 }
 
 output "secret_arn" {
